03ee4785d1f664e0aa0a8299e92a4f4e82af6283
aashik <729aashik729@gamil.com>
2025-07-14 16:20:33 +0200

    added buzzer code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from messaging.publish_sensor_data import publish_sensor_data
+from sensors.buzzer_sensor import activate_buzzer_sensor
 from sensors.dht_sensor import read_dht_sensor
 
 
@@ -7,7 +8,8 @@ from sensors.dht_sensor import read_dht_sensor
 def main():
     print("Starting data collection...")
     # Read from DHT sensor
-    dht_data = read_dht_sensor()    
+    dht_data = read_dht_sensor() 
+    activate_buzzer_sensor()   
     publish_sensor_data(dht_data)
     print("DHT data published.")
 if __name__ == "__main__":
---
+++ sensors/buzzer_sensor.py
@@ -0,0 +1,21 @@
+import grovepi
+import time
+
+BUZZER_PORT = 2    # Buzzer connected to digital port D2
+
+def activate_buzzer_sensor():
+    try:
+        # Read temperature and humidity from DHT sensor
+
+
+        # Activate buzzer for 2 seconds
+        grovepi.pinMode(BUZZER_PORT, "OUTPUT")
+        grovepi.digitalWrite(BUZZER_PORT, 1)
+        time.sleep(2)
+        grovepi.digitalWrite(BUZZER_PORT, 0)
+
+    except IOError as e:
+        return {
+            "error": "Failed to read from DHT sensor or control buzzer",
+            "message": str(e)
+        }
