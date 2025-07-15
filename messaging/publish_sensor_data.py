1b5780ceac53dc7a86d98db2fe1e2ccb7e56b0ed
aashik <729aashik729@gamil.com>
2025-07-15 16:24:19 +0200

    upadated the publish senor data

--- messaging/publish_sensor_data.py
+++ messaging/publish_sensor_data.py
@@ -1,15 +1,21 @@
+from messaging.publisher import publish_message
+
+
 def publish_sensor_data(data, topic):
     if topic == "sensor.dht.environment":
         msg = "{} | DHT Temp = {}°C, Humidity = {}%".format(
             data['timestamp'], data['temperature'], data['humidity']
         )
+        publish_message(topic, msg)
     elif topic == "sensor.pir.environment":
         msg = "{} | Motion Detected = {}".format(
             data.get('timestamp', 'N/A'), data.get('motion_detected')
         )
+        publish_message(topic, msg)
     else:
         msg = "{} | Data: {}".format(
             data.get('timestamp', 'N/A'), data
         )
+        publish_message(topic, msg)
 
     print("Publishing to {}: {}".format(topic, msg))
